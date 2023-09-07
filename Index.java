c35ddb7aac91783befbc8dde52148927014ee48b
Sarah <forsugar88@gmail.com>
2023-09-07 11:17:33 -0700

    saves to the right folder

--- Index.java
+++ Index.java
@@ -1,4 +1,5 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -7,8 +8,18 @@ public class Index
     Boolean fileExists;
     public Index() throws IOException //should NOT put an entry twice
     {
+        initialize();
+        //Somewhere I need to initialize objects folder and index file
         BufferedWriter bw = new BufferedWriter(new FileWriter("index"));
         bw.write("string name" + " : " + "SHA1 name" + "\n"); //MAKE THIS CORRECT
         bw.close();
+    }
+
+    public void initialize()
+    {
+        fileExists = true;
+        new File("objects").mkdirs();
+
+
     }
 }
--- tester.java
+++ tester.java
@@ -4,6 +4,8 @@ public class tester
 {
     public static void main(String[] args) throws IOException
     {
+        Index indexTester = new Index();
+        indexTester.initialize();
         Blob blobTester = new Blob("testerText");
     }
     
