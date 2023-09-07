0fc0a1b1a599c183ba8648db8536cbfe6b67d5ef
Sarah <forsugar88@gmail.com>
2023-09-07 09:17:27 -0700

    Done with SHA1 conversion

--- Blob.java
+++ Blob.java
@@ -3,30 +3,62 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.nio.file.*;
 
 public class Blob 
 { 
-
-    static final Path myPath = Paths.get("./objects/");
-
-    public static String toSHA1(byte[] b) {
-        String result = "";
-        for (int i=0; i < b.length; i++) {
-          result +=
-                Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+    private static String encryptPassword(String password)
+    {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(password.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
         }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
         return result;
-      }
+    }
+      
 
     public Blob(String fileName) throws IOException
     {
-        byte[] asBytes = fileName.getBytes();
-        String newFileName = toSHA1(asBytes);
+        BufferedReader ogReader= new BufferedReader(new BufferedReader(new FileReader(fileName))); 
+        StringBuilder sb = new StringBuilder();
+        while(ogReader.ready())
+        {
+            sb.append((char)ogReader.read());
+        }
+        byte[] asBytes = sb.toString().getBytes();
+        //String newFileName = toSHA1(asBytes);
+        ogReader.close();
 
-        
+        String newFileName = Blob.encryptPassword(fileName);
+        System.out.print("FILE NAME: " + newFileName);
         //how do you save a file in the objects folder??
         BufferedReader br = new BufferedReader(new BufferedReader(new FileReader(fileName))); 
         BufferedWriter bw = new BufferedWriter(new FileWriter(newFileName));
