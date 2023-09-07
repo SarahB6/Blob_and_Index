2d5f0c7bce12fce3eecd296bb643e2123f5c810d
Sarah <forsugar88@gmail.com>
2023-09-06 17:30:05 -0700

    copied in toSHA1 code

--- Blob_and_Index.java
+++ Blob_and_Index.java
@@ -1,5 +1,20 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Blob_and_Index 
 {
+    private static String toSHA1(byte[] convertme) //ask if it needs to be static
+    {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } 
+        return new String(md.digest(convertme));
+    }
+
     public void Blob()
     {
 
@@ -7,6 +22,6 @@ public class Blob_and_Index
 
     public void Index()
     {
-        
+
     }
 }
