e6f1f9c654651bd1a881afdbde5bd0cf2a8edbc5
Oren Hartstein <orenh2020@gmail.com>
2023-09-19 14:57:14 -0700

    added getDate to commit

--- Commit.java
+++ Commit.java
@@ -1,16 +1,23 @@
 public class Commit {
-    private String nextSHA1, author, summary;
+    private String nextSHA1, author, summary, date;
 
     //constructor with nextSHA1
     public Commit(String nextSHA1, String author, String summary){
         this.nextSHA1 = nextSHA1;
         this.author = author;
         this.summary = summary;
+        this.date = getDate();
     }
 
     //constructor without nextSHA1
     public Commit(String author, String summary){
         this.author = author;
         this.summary = summary;
+        this.date = getDate();
+    }
+
+    //returns the current date in YYYY-MM-DD
+    public String getDate(){
+        return java.time.LocalDate.now().toString();
     }
 }
