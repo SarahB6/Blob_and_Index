72e394d3865de345652dd5f99e618cdf298961c5
Oren Hartstein <orenh2020@gmail.com>
2023-09-21 18:29:24 -0700

    added createTree method to Commit

--- Commit.java
+++ Commit.java
@@ -1,16 +1,20 @@
+import java.io.IOException;
+
 public class Commit {
-    private String nextSHA1, author, summary, date;
+    private String treeSha1,nextSha1, author, summary, date;
 
     //constructor with nextSHA1
-    public Commit(String nextSHA1, String author, String summary){
-        this.nextSHA1 = nextSHA1;
+    public Commit(String nextSha1, String author, String summary) throws IOException{
+        this.treeSha1 = createTree();
+        this.nextSha1 = nextSha1;
         this.author = author;
         this.summary = summary;
         this.date = getDate();
     }
 
     //constructor without nextSHA1
-    public Commit(String author, String summary){
+    public Commit(String author, String summary) throws IOException{
+        this.treeSha1 = createTree();
         this.author = author;
         this.summary = summary;
         this.date = getDate();
@@ -19,5 +23,12 @@ public class Commit {
     //returns the current date in YYYY-MM-DD
     public String getDate(){
         return java.time.LocalDate.now().toString();
+    }
+
+    //creates and saves an empty tree to the objects folder, returns the sha of the tree
+    public String createTree() throws IOException{
+        Tree tree = new Tree();
+        tree.save();
+        return tree.getSha1();
     }
 }
